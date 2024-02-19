5d6fd72ec8eb37721e174c95f2e24493e961147e
MWANZIA <shem.masoo@student.moringaschool.com>
2024-02-19 14:28:35 +0300

    app and models

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask
+from flask_restful import Api
 from flask_migrate import Migrate
 from models import db
+
 
 app= Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] ='sqlite:///data.db'
@@ -12,5 +14,10 @@ migrations=Migrate(app,db)
 
 db.init_app(app)
 
+api=Api(app)
+ 
+
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
+
+  
