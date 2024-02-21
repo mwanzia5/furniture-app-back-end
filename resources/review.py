b42cf71631ea58d7665ea751a23e04e6f0f15fe3
MWANZIA <shem.masoo@student.moringaschool.com>
2024-02-21 22:11:28 +0300

    error sorted

--- resources/review.py
+++ resources/review.py
@@ -22,51 +22,30 @@ class ReviewList(Resource):
     reviews_parser.add_argument('text', required=True, help='Text is required')
     reviews_parser.add_argument('rating', required=True, help='Rating is required')
 
-
     @marshal_with(reviews_fields)
     def get(self):
         reviews = ReviewModel.query.all()
         return reviews
-    
-    @marshal_with(response_field)
-    def post(self):
-        data = ReviewList.query.all()
+
+
 class Review_id(Resource):
     reviews_parser = reqparse.RequestParser()
     reviews_parser.add_argument('text', required=True, help='Text is required')
     reviews_parser.add_argument('rating', required=True, help='Rating is required')
 
-@marshal_with(reviews_fields)
-def review_by_id(id):
-    review = ReviewModel.query.filter_by(id=id).first()
+    @marshal_with(reviews_fields)
+    def get(self, id):
+        review = ReviewModel.query.get(id)
+        return review
 
-    if request.method == 'GET':
-        review_dict = review.to_dict()
+    @marshal_with(reviews_fields)
+    def post(self, id):
+        args = self.reviews_parser.parse_args()
+        review = ReviewModel.query.filter_by(id=args['id']).first()
 
-        response = make_response(
-            jsonify(review_dict),
-            200
-        )
-
-        return response
-
-    elif request.method == 'POST':
-         new_review = ReviewModel(
-            id=request.form.get("id"),
-            user_id=request.form.get("user_id"),
-            product_id=request.form.get("product-id"),
-            text=request.form.get("text"),
-            rating=request.form.get("rating")
-        )
-
-    db.session.add(new_review)
-    db.session.commit()
-
-    review_dict = new_review.to_dict()
-
-    response = make_response(
-            jsonify(review_dict),
-            201
-        )
-
-    return response
+        if review == ReviewModel:
+            abort(400, message="Review not found")
+            
+        db.session.add(review)
+        db.session.commit()
+        return review, 201
